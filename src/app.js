6adaf3f71d8cf3c81d1277b6b7616863405d1b81
Leonardo Medina <leonardomedina1250@gmail.com>
2024-10-02 19:08:51 +0000

    1 en proceso

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap"; // Descomentar si usas Bootstrap
+// // import "./style.css"; // Descomentar si usas CSS
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// write your code here
+function excusasF1() {
+  const quien = [
+    "Fernando Alonso",
+    "Max Verstappen",
+    "Lewis Hamilton",
+    "Lando Norris",
+    "Sergio Pérez",
+    "Carlos Sainz Jr"
+  ];
+  const accion = [
+    " chocó con ",
+    " pasó por la izquierda de ",
+    " pasó por la derecha de ",
+    " esquivó en el último minuto a "
+  ];
+  const cosa = [
+    "un mecánico",
+    "un neumático",
+    "un rezagado",
+    "un aficionado que saltó a la pista"
+  ];
+  const lugar = [
+    " en Imola.",
+    " en Suzuka.",
+    " en Monza.",
+    " en Barcelona.",
+    " en Nürburgring.",
+    " en Silverstone."
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Selección aleatoria de los elementos
+  const quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+  const accionAleatorio = accion[Math.floor(Math.random() * accion.length)];
+  const cosaAleatorio = cosa[Math.floor(Math.random() * cosa.length)];
+  const lugarAleatorio = lugar[Math.floor(Math.random() * lugar.length)];
+
+  // Concatenación de la excusa final
+  const excusaFinal =
+    quienAleatorio + accionAleatorio + cosaAleatorio + lugarAleatorio;
+
+  // Mostrar el resultado en el elemento con id "excusaFinal"
+  document.getElementById("excusaFinal").innerText = excusaFinal;
+
+  return excusaFinal;
+}
